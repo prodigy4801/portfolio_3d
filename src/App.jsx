f6ca9746be82f10e7aa41241f5ab84b2e537ea8b
Samuel Esezobor <samuel.esezobor076@gmail.com>
2025-03-04 04:47:36 +0100

    Commit Initial project scaffolding

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Samuel Esezobor 3D Portfolio Application</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
